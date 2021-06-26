ee7c80998f0c937913d3f235575e97157768876c
DSR! <xchwarze@gmail.com>
2021-06-26 02:49:46 -0300

    Update CKEditor to 4.16.1

--- dsr/ckeditor/styles/all/template/js/ckeditor/build-config.js
+++ dsr/ckeditor/styles/all/template/js/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/a802780ae924a13d2d7eee22b8d3131b
+ * (2) https://ckeditor.com/cke4/builder/611a13ce5c848f61db4adb551384e0f4
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/a802780ae924a13d2d7eee22b8d3131b
+ * (3) https://ckeditor.com/cke4/builder/download/611a13ce5c848f61db4adb551384e0f4
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -39,6 +39,7 @@ var CKBUILDER_CONFIG = {
 		'.jshintrc',
 		'.mailmap',
 		'.npm',
+		'.nvmrc',
 		'.travis.yml',
 		'bender-err.log',
 		'bender-out.log',
